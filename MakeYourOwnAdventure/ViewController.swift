6a55b473fa9a91385d29fc43eef289044c6a77de
Yeontae Kim <ytk007@gmail.com>
2017-04-28 14:09:22 -0700

    Initial commit

---
+++ MakeYourOwnAdventure/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MakeYourOwnAdventure
+//
+//  Created by Yeontae Kim on 4/28/17.
+//  Copyright © 2017 YTK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
